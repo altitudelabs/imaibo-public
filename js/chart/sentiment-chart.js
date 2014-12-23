d6cc59653c471312b1198f149b99f9fc5c1799c9
Ray Tung <raytung@users.noreply.github.com>
2014-12-23 21:58:09 +0800

    Revert "Revert "Sentiment range""

--- js/chart/sentiment-chart.js
+++ js/chart/sentiment-chart.js
@@ -1,70 +1,106 @@
 var SentimentChart = {
-  build: function(){
+  properties: {},
+  setProperties: function(options) {
+    var properties = {
+      height: 244,
+      interval: 40,
+      margin: { top: 6, right: 45, bottom: 36, left: 45 },
+    };
+    if (options) {
+      for (var key in options) {
+        properties[key] = options[key];
+      }
+    }
+    this.properties = $.extend(true, {}, properties);
+  },
+  init: function(){
+    this.setProperties();
+    this.drawContainer();
+    this.drawGraph(true);
+  },
+  drawContainer: function(){
     $('#sentiment-chart').empty();
+    $('#sentiment-chart-label').empty();
 
-    var data = ChartView.data,
-    width = ChartView.properties.width,
-    height = 200,
-    margin = { top: 8, right: 45, bottom: 30, left: 45 },
-    interval = 40;
+    var data = ChartView.data;
+    var containerWidth = ChartView.properties.width;
+    var margin = this.properties.margin;
+    var chartWidth = containerWidth - margin.left - margin.right;
 
-    var chart = d3.select('#sentiment-chart')
-    .append('svg:svg')
-    .attr('class', 'chart')
-    .attr('width', width)
-    .attr('height', height);
+    var height = this.properties.height;
+    var chartHeight = height - margin.top - margin.bottom;
+    var interval = this.properties.interval;
 
     var sentimentData = data.sentiment.moodindexList.slice(0, 8);
 
     var y1 = d3.scale.linear()
-    .domain([d3.min(sentimentData.map(function(x) { return +x.mood; })), d3.max(sentimentData.map(function(x){return +x.mood; }))])
-    .range([height-margin.bottom-20, margin.top]);
+    .domain([-1 , 1])
+    .range([chartHeight-margin.bottom, margin.top+20]);
 
     var y2 = d3.scale.linear()
     .domain([d3.min(data.sentiment.indexList.map(function(x) { return +x.price; })), d3.max(data.sentiment.indexList.map(function(x){return +x.price; }))])
-    .range([height-margin.bottom, margin.top]);
+    .range([chartHeight-margin.bottom, margin.top+20]);
 
     var seven_am = sentimentData[0].timestamp; //seven_am in epoch time
     var min_before_midnight = seven_am + 61140; //the same day as seven_am
 
     var x = d3.scale.linear()
     .domain([seven_am, min_before_midnight])
-    .range([margin.left, width-margin.right ]);
+    .range([margin.left, chartWidth + margin.left ]);
 
-    chart.append('svg:line')
-    .attr('class', 'xaxis')
-    .attr('x1', margin.left)
-    .attr('x2', width - margin.right)
-    .attr('y1', height - margin.bottom)
-    .attr('y2', height - margin.bottom)
-    .attr('stroke', '#464646');
+    var chart_label = d3.select('#sentiment-chart-label')
+    .append('svg:svg')
+    .attr('class', 'chart')
+    .attr('width', containerWidth + 120)
+    .attr('height', chartHeight);
 
-    chart.append('svg:line')
+    $('#sentiment-chart-container').slimScroll({
+      height: (chartHeight+20).toString() + 'px',
+      width: chartWidth.toString() + 'px',
+      color: '#ffcc00'
+    });
+
+    $('#sentiment-chart-container').css('top', 0);
+
+    $('#sentiment > .slimScrollDiv')
+    .css('position', 'absolute')
+    .css('top', '39px')
+    .css('left', '45px')
+    .css('width', chartWidth.toString() + 'px');
+
+    chart_label.append('svg:line')
     .attr('class', 'xborder-top-thick')
     .attr('x1', margin.left)
-    .attr('x2', width - margin.right)
+    .attr('x2', chartWidth + margin.left)
     .attr('y1', margin.top)
     .attr('y2', margin.top)
     .attr('stroke', '#464646');
 
-    chart.append('g')
-    .attr('class','xlabels')
-    .selectAll('text.xrule')
-    .data(x.ticks(400))
-    .enter().append('svg:text')
-    .attr('class', 'xrule')
-    .attr('x', function(i){ return x(i); })
-    .attr('y', height-margin.bottom+20)
-    .attr('text-anchor', 'middle')
-    .text(function(i){
-      if(i%3600===0){
-        var d = new Date(i*1000);
-        // d.getDate() + '-' +
-        return d.getHours() + ':' + (d.getMinutes() < 10? '0' + d.getMinutes(): d.getMinutes());
-      }
-    });
+    chart_label.append('svg:line')
+    .attr('class', 'yborder-left')
+    .attr('x1', margin.left)
+    .attr('x2', margin.left)
+    .attr('y1', chartHeight - margin.bottom)
+    .attr('y2', margin.top)
+    .attr('stroke', '#464646');
 
-    chart.append('g')
+    chart_label.append('svg:line')
+    .attr('class', 'yborder-right')
+    .attr('x1', chartWidth + margin.left)
+    .attr('x2', chartWidth + margin.left)
+    .attr('y1', chartHeight - margin.bottom)
+    .attr('y2', margin.top)
+    .attr('stroke', '#464646');
+
+    chart_label.append('svg:line')
+    .attr('class', 'xaxis')
+    .attr('x1', margin.left)
+    .attr('x2', containerWidth - margin.right)
+    .attr('y1', chartHeight - margin.bottom)
+    .attr('y2', chartHeight - margin.bottom)
+    .attr('stroke', '#464646');
+
+    chart_label.append('g')
     .attr('class','y1labels')
     .selectAll('text.yrule')
     .data(y1.ticks(5))
@@ -75,46 +111,79 @@ var SentimentChart = {
     .attr('text-anchor', 'middle')
     .text(String);
 
-    chart.append('svg:line')
-    .attr('class', 'yborder-left')
-    .attr('x1', margin.left)
-    .attr('x2', margin.left)
-    .attr('y1', margin.top)
-    .attr('y2', height-margin.bottom)
-    .attr('stroke', '#464646');
-
-    chart.append('svg:line')
-    .attr('class', 'yborder-right')
-    .attr('x1',  width - margin.right - 1)
-    .attr('x2', width - margin.right - 1)
-    .attr('y1', margin.top)
-    .attr('y2', height-margin.bottom)
-    .attr('stroke', '#464646');
-
-    chart.append('g')
+    chart_label.append('g')
     .attr('class','y2labels')
     .selectAll('text.yrule')
     .data(y2.ticks(5))
     .enter().append('svg:text')
     .attr('class', 'yrule')
-    .attr('x', width-margin.right + 18)
+    .attr('x', chartWidth + margin.left + 18)
     .attr('y', y2)
     .attr('text-anchor', 'middle')
     .text(String);
 
-    // chart.append('g')
-    // .attr('class','ylines')
-    // .selectAll('line.y1')
-    // .data(y1.ticks(5))
-    // .enter().append('svg:line')
-    // .attr('class', 'y1')
-    // .attr('x1', margin.left)
-    // .attr('x2', width - margin.right)
-    // .attr('y1', y1)
-    // .attr('y2', y1)
-    // .attr('stroke', '#464646')
-    // .attr('stroke-width', '0.3px');
+  },
+  drawGraph: function(isNew){
+    'use strict';
 
+    $('#sentiment-chart').empty();
+
+    var data = ChartView.data,
+    prop = ChartView.properties,
+    margin = prop.margin,
+    chartWidth  = prop.width - margin.left - margin.right,
+    height      = this.properties.height,
+    chartHeight = height - margin.top - margin.bottom,
+    zoomFactor     = prop.zoomFactor,
+    graphWidth     = chartWidth * zoomFactor,
+    data           = ChartView.data,
+    interval       = this.properties.interval,
+    xlabels,
+    gline,
+    tooltip;
+
+    var sentimentData = data.sentiment.moodindexList.slice(0, 8);
+
+    var y1 = d3.scale.linear()
+    .domain([-1 , 1])
+    .range([chartHeight+margin.top-57, margin.top]);
+
+    var y2 = d3.scale.linear()
+    .domain([d3.min(data.sentiment.indexList.map(function(x) { return +x.price; })), d3.max(data.sentiment.indexList.map(function(x){return +x.price; }))])
+    .range([147, 27]);
+
+    console.log();
+
+
+    var seven_am = sentimentData[0].timestamp; //seven_am in epoch time
+    var min_before_midnight = seven_am + 61140; //the same day as seven_am
+
+    var x = d3.scale.linear()
+    .domain([seven_am, min_before_midnight])
+    .range([margin.left, chartWidth-margin.right ]);
+
+    var chart = d3.select('#sentiment-chart')
+    .append('svg:svg')
+    .attr('class', 'chart')
+    .attr('width', chartWidth)
+    .attr('height', chartHeight);
+
+    chart.append('g')
+    .attr('class','xlabels')
+    .selectAll('text.xrule')
+    .data(x.ticks(400))
+    .enter().append('svg:text')
+    .attr('class', 'xrule')
+    .attr('x', function(i){ return x(i); })
+    .attr('y', chartHeight-margin.bottom-margin.top)
+    .attr('text-anchor', 'middle')
+    .text(function(i){
+      if(i%3600===0){
+        var d = new Date(i*1000);
+        // d.getDate() + '-' +
+        return d.getHours() + ':' + (d.getMinutes() < 10? '0' + d.getMinutes(): d.getMinutes());
+      }
+    });
     var sentimentLine = d3.svg.line()
     .x(function(d,i) {
       return x(d.timestamp); })
@@ -193,6 +262,7 @@ var SentimentChart = {
       .attr('d', securityLine)
       .attr('stroke', '#fff')
       .attr('fill', 'none');
+
   },
 };
 
