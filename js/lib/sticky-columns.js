f7f9b48fa5ca0c0096659014cf29012116499c7c
Eugene Choi <eugene.maestro.choi@gmail.com>
2015-01-25 20:50:08 +0800

    stop sticky on IE8

--- js/lib/sticky-columns.js
+++ js/lib/sticky-columns.js
@@ -1,6 +1,7 @@
 var StickyColumns = {
   active: false,
   start: function(){
+    if (IE8) { return; }
     if (!this.active) {
       this.active = true;
       $('#right-panel, #content').stick_in_parent();
@@ -11,10 +12,12 @@ var StickyColumns = {
     }
   },
   stop: function(){
+    if (IE8) { return; }
     $('#right-panel, #content').trigger('sticky_kit:detach');
     this.active = false;
   },
   recalc: function(){
+    if (IE8) { return; }
     $('#right-panel, #content').trigger('sticky_kit:recalc');
     // $('.container').scrollLeft(ChartView.properties.scrollDistance);
   }
