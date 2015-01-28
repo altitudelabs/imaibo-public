b01319be8a9159271e6b83faf113013396564192
PMLaw <miruappy@gmail.com>
2015-01-28 15:21:37 +0800

    right scrollbar removed

--- js/lib/double-scrollbars.js
+++ js/lib/double-scrollbars.js
@@ -3,7 +3,6 @@ var DoubleScrollbars = {
     this.displayScrollbar('#news-view', '.scroller', '.scroller__bar');
     this.displayScrollbar('#experts-view', '.scroller', '.scroller__bar');
     this.displayScrollbar('#stockpicker-view', '.scroller', '.scroller__bar');
-    this.displayScrollbar('.outer', '#outer-scroller', '#outer-scroller__bar');
   },
   displayScrollbar: function(root, scroller, bar) {
     var params = {
