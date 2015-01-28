d844ed6caaf63b1c9833b23947594154abe2aaae
PMLaw <miruappy@gmail.com>
2015-01-28 10:36:38 +0800

    double scrolling done
    changes to view css
    
    news view scrolling
    
    expert view
    
    stockpicker scroller
    
    minor fix
    
    code clean up
    
    more amendment
    
    delete files
    
    remove clipping
    
    change height function added
    
    right scrollbar removed
    
    making scrollbar to the right
    
    add padding
    
    making the whole page scrollable all the time
    
    display scrollbar only while hovering the corr view
    
    bottom border
    
    add panel shows
    
    fix animation
    
    adding the bottom padding for news back
    
    fix tooltip truncate problem
    
    replaced calc in style.css to IE8 compatible version
    
    added scrollbar back
    
    fixed settings table

---
+++ js/lib/double-scrollbars.js
@@ -0,0 +1,24 @@
+var DoubleScrollbars = {
+  start: function() {
+    this.displayScrollbar('#news-view', '.scroller', '.scroller__bar');
+    this.displayScrollbar('#experts-view', '.scroller', '.scroller__bar');
+    this.displayScrollbar('#stockpicker-view', '.scroller', '.scroller__bar');
+  },
+  displayScrollbar: function(root, scroller, bar) {
+    var params = {
+      root: root,
+      scroller: scroller,
+      bar: bar,
+      barOnCls: 'baron'
+    };
+
+    baron(params);
+
+    // show scrollbar only while hovering the corr view
+    var scrollbarSelector = root + ' ' + bar;
+    $(root).hover(
+      function() { $(scrollbarSelector).fadeTo(800, 1); },
+      function() { $(scrollbarSelector).fadeTo(800, 0); }
+    );
+  }
+};
